b8b884e5a0dd232fc636a11739af4b58ad75983f
ArfySaysHi <necroticowl@gmail.com>
2026-04-07 16:41:15 +0100

    performance issue fixed via locking regex

--- src-tauri/src/helpers/string_helper.rs
+++ src-tauri/src/helpers/string_helper.rs
@@ -1,6 +1,9 @@
 use regex::Regex;
+use std::sync::OnceLock;
+
+static ANSI_RE: OnceLock<Regex> = OnceLock::new();
 
 pub fn strip_ansi(s: &str) -> String {
-    let re = Regex::new(r"\x1b\[[0-?]*[ -/]*[@-~]").unwrap();
+    let re = ANSI_RE.get_or_init(|| Regex::new(r"\x1b\[[0-?]*[ -/]*[@-~]").unwrap());
     re.replace_all(s, "").into_owned()
 }
