526b0f3e5f136a9b8120ca03ca6d6dbecfe0442c
ArfySaysHi <necroticowl@gmail.com>
2026-04-15 17:13:29 +0100

    drag and drop refactor to avoid doubled archives

--- src/hooks/useDragDrop.ts
+++ src/hooks/useDragDrop.ts
@@ -10,25 +10,29 @@ export default function useDragDrop(
 
   useEffect(() => {
     let unlisten: (() => void) | undefined;
+    let mounted = true;
 
-    const setup = async () => {
-      unlisten = await getCurrentWebview().listen(
+    (async () => {
+      const listener = await getCurrentWebview().listen(
         "tauri://drag-drop",
         async (event) => {
           const { paths } = event.payload as { paths: string[] };
 
           for (const path of paths) {
-            if (path.toLowerCase().endsWith(".mpq")) {
+            if (path.toLowerCase().endsWith(".mpq"))
               await managerRef.current.openMpq(path);
-            } else {
-              await managerRef.current.addFile(path);
-            }
+            else await managerRef.current.addFile(path);
           }
         },
       );
-    };
 
-    setup();
-    return () => unlisten?.();
+      if (mounted) unlisten = listener;
+      else listener();
+    })();
+
+    return () => {
+      mounted = false;
+      if (unlisten) unlisten();
+    };
   }, []);
 }
